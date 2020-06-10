bc572ad061db1ac6d1a2fc87ceb6b8e78e8e7ec6
Marc Delalonde <marcdelalonde@gmail.com>
2020-06-10 11:23:26 +0200

    Update cors config

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class AdminMate {
   }
 
   accessControl(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3002');
+    res.header('Access-Control-Allow-Origin', 'https://my.adminmate.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', true);
