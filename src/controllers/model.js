9d4123dc9c88c83eb1ecdd98c960ebbc777ca7e5
Marc Delalonde <marcdelalonde@gmail.com>
2021-01-18 21:47:42 +0100

    Add month & year timeframe for bar/line charts

--- src/controllers/model.js
+++ src/controllers/model.js
@@ -375,6 +375,22 @@ module.exports.customQuery = async (req, res) => {
       };
       groupFormat = '%V';
     }
+    // Month timeframe
+    else if (data.timeframe === 'month') {
+      matchReq = {
+        '$gte': new Date(global._moment().subtract(12, 'month').startOf('month').format()),
+        '$lte': new Date(global._moment().endOf('month').format())
+      };
+      groupFormat = '%m';
+    }
+    // Year timeframe
+    else if (data.timeframe === 'year') {
+      matchReq = {
+        '$gte': new Date(global._moment().subtract(8, 'year').startOf('year').format()),
+        '$lte': new Date(global._moment().endOf('year').format())
+      };
+      groupFormat = '%Y';
+    }
 
     if (!groupFormat) {
       return res.status(403).json({ message: 'Invalid request' });
@@ -423,6 +439,30 @@ module.exports.customQuery = async (req, res) => {
         const countForTheTimeframe = global._.find(repartitionData, { key: currentWeek.format('WW') });
         formattedData.push({
           key: currentWeek.startOf('week').format('DD/MM'),
+          value: countForTheTimeframe ? countForTheTimeframe.value : 0
+        });
+      }
+    }
+    // Month timeframe
+    else if (data.timeframe === 'month') {
+      for (let i = 0; i < 12; i++) {
+        const currentMonth = global._moment().subtract(i, 'month');
+
+        const countForTheTimeframe = global._.find(repartitionData, { key: currentMonth.format('MM') });
+        formattedData.push({
+          key: currentMonth.startOf('month').format('MMM'),
+          value: countForTheTimeframe ? countForTheTimeframe.value : 0
+        });
+      }
+    }
+    // Year timeframe
+    else if (data.timeframe === 'year') {
+      for (let i = 0; i < 8; i++) {
+        const currentYear = global._moment().subtract(i, 'year');
+
+        const countForTheTimeframe = global._.find(repartitionData, { key: currentYear.format('YYYY') });
+        formattedData.push({
+          key: currentYear.startOf('year').format('YYYY'),
           value: countForTheTimeframe ? countForTheTimeframe.value : 0
         });
       }
