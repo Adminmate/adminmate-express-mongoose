0fe09cf8fe1aeb7115fdc8a7e4257b071c819121
Marc Delalonde <marcdelalonde@gmail.com>
2021-11-02 22:39:47 +0200

    Fix tests

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -17,3 +17,9 @@ require('./app.js');
 
 require('./tests/model-getall.test.js');
 require('./tests/model-query.test.js');
+
+// Close mongoose connection
+afterAll(async () => {
+  const mongoose = require('mongoose');
+  await mongoose.connection.close();
+});
